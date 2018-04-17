db5767f90f7cc11004b810ae89f36e9d458d97df
russellsb <r.sammutbonnici@gmail.com>
2018-04-17 23:58:35 +0200

    SLD Derivation back-chaining for one atom clauses Also changed variable bool to polarity

--- task1.py
+++ task1.py
@@ -2,9 +2,9 @@
 class Literal:
 
     # initializes the literal object
-    def __init__(self, name="", bool=True):
+    def __init__(self, name="", polarity=True):
         self.name = name # Contains the String name of the literal
-        self.bool = bool  # Whether literal is true or false
+        self.polarity = polarity  # Whether literal is true or false
 
 
 # class hornClause for storing clause as an object
@@ -18,10 +18,12 @@ class HornClause:
         self.literalList.append(literal)
 
 
-# method that parses text file, and from which it produces a list of horn clauses called "clauseList"
-def textToClauses(fileName):
+# method that parses text file, and from which it produces a list of horn clauses called "formula"
+def textToKnowledgeBase(fileName):
 
-    clauseList = []  # stores list of clauses, that in turn stores literals
+    formula = []  # stores list of clauses, that in turn stores literals
+
+    print "Scanning text file: \n--------\n"
 
     # opens file
     f = open(fileName,"r")
@@ -33,30 +35,29 @@ def textToClauses(fileName):
         if not ch: break  # breaks when no longer holds true
 
         if(ch == '['):
-            tempC = HornClause()  # initialize new temporary horn clause
+            queryC = HornClause()  # initialize new temporary horn clause
             tempL = Literal()  # initialize new temporary literal
 
         elif (ch == '!'):
-            tempL.bool = False  # sets boolean value to false
+            tempL.polarity = False  # sets boolean value to false
 
         elif (ch == ','):
-            tempC.addLiteral(tempL)   # add finished literal to object
+            queryC.addLiteral(tempL)   # add finished literal to object
             tempL = Literal()  # initialize new temporary literal
 
         elif (ch == ']'):
-            tempC.addLiteral(tempL)  # add finished literal to object
-            clauseList.append(tempC)   # append finished object to list
+            queryC.addLiteral(tempL)  # add finished literal to object
+            formula.append(queryC)   # append finished object to list
 
         elif (ch == '\n'):
 
             pass  # condition used to catch '\n' so it doesn't get detected as alphanumeric and put in name attribute
 
-            ''' UNCOMMENT TO PRINT CONTENTS PER CLAUSE
-            for i in range(len(tempC.literalList)): # loops through whole literalList of current finished clause
-                print "%s, %s"%(tempC.literalList[i].bool,tempC.literalList[i].name)  # prints True/False, and name
+            for i in range(len(queryC.literalList)): # loops through whole literalList of current finished clause
+                print "%s, %s"%(queryC.literalList[i].polarity,queryC.literalList[i].name)  # prints True/False, and name
 
             print ""  # skips line for next clause
-            '''
+
 
         elif (ch.isalpha):
             tempL.name += ch   # concatenates character to literal string name
@@ -64,7 +65,72 @@ def textToClauses(fileName):
         else:
             print "Error: unrecognized character in text file \"%s\""%fileName
 
-    return clauseList  # returns list of horn clauses
+    print "--------\nKnowledge base has been loaded.\n"
+
+    return formula  # returns list of horn clauses
 
 
-hcList = textToClauses("clauseStatements.txt")   # stores all the clauses from "clauseStatements.txt" into hcList
+# method to parse string query to clause object, to later be used for SLD back-chaining
+def requestQuery():
+
+    queryString = raw_input("Please input a query clause to resolve from the Knowledge Base (Use ! for negation): ")
+    print ""  # skips a line
+
+    # parses queryString, storing all the information in an object clause "queryC"
+    for ch in queryString:
+
+        if (ch == '['):
+            queryC = HornClause()  # initialize query horn clause
+            tempL = Literal()  # initialize new temporary literal
+
+        elif (ch == '!'):
+            tempL.polarity = False  # sets boolean value to false
+
+        elif (ch == ','):
+            queryC.addLiteral(tempL)  # add finished literal to object
+            tempL = Literal()  # initialize new temporary literal
+
+        elif (ch == ']'):
+            queryC.addLiteral(tempL)  # add finished literal to object
+
+        elif (ch.isalpha):
+            tempL.name += ch  # concatenates character to literal string name
+
+        else:
+            print "Error: unrecognized character in user input string \"%s\"\n" % queryString
+
+    return queryC
+
+
+# recursive method that returns whether the query is solvable or unsolvable
+def _resolve(knowledgeBase, query):
+
+    # checks if query at depth is empty
+    if(len(query.literalList)==0):
+        print "SOLVED"
+        return True
+
+    # traverses through clauses in the knowledgeBase
+    for clause in knowledgeBase:
+
+        # traverses through literals in clause
+        for literal in clause.literalList:
+
+                # checks whether same literal as query is found, with opposite polarity
+                if (literal.name == query.literalList[0].name) and (literal.polarity != query.literalList[0].polarity):
+
+                    print"Found: %s, (Query: %s)" % (literal.name, query.literalList[0].name)
+                    query.literalList.remove(query.literalList[0])
+                    _resolve(knowledgeBase, query)
+                    return True
+
+    print "NOT SOLVED"
+    return False
+
+
+hcFormula = textToKnowledgeBase("clauseStatements.txt")   # stores all the clauses from "clauseStatements.txt" into hcFormula
+query = requestQuery()
+_resolve(hcFormula, query)
+
+
+
